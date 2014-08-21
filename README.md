7597c68145f6d78a63d81ac218667729fe1a70cd
Jonathan Puckey <me@jonathanpuckey.com>
2014-08-21 23:12:52 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,68 @@
-undies
+Undies
 ======
 
-An Undo / Redo history module for JavaScript
+A simple undo / redo history module for Javascript.
+
+Based on the Undo prototype in Mikko Mononen's [Stylii](https://github.com/memononen/stylii/blob/master/js/editor.js#L2)
+
+Copyright (c) 2014 Moniker
+http://studiomoniker.com
+
+## Example
+
+```javascript
+// Your data:
+var text = '';
+
+var undies = new Undies({
+    // The maximum amount of undos:
+    maxUndos: 8,
+
+    // Called whenever a snapshot is made in order to serialize your data:
+    serialize: function() {
+        // Clone the current state of the text string:
+        return '' + text;
+    },
+
+    // Whenever undies.undo() or undies.redo() is called,
+    // the restore function is called with the restored
+    // data:
+    restore: function(data) {
+        // Unserialize the data from the undo history and replace
+        // your your original data with it:
+        text = data;
+    }
+});
+
+console.log('Adding a to text');
+text += 'a';
+undies.snapshot('added A');
+
+console.log('Adding b to text');
+text += 'b';
+undies.snapshot('added B');
+
+console.log('Adding c to text');
+text += 'c';
+undies.snapshot('added C');
+
+console.log('Text now contains:', text);
+
+console.log('Undo once');
+undies.undo();
+
+console.log('Text now contains:', text); // ab
+
+console.log('Undo once');
+undies.undo();
+
+console.log('Text now contains:', text); // a
+
+console.log('Redo once');
+undies.redo();
+
+console.log('Text now contains:', text); // ab
+
+```
+
+Distributed under the MIT license.
---
+++ Undies.js
@@ -0,0 +1,80 @@
+/*
+ * Undies: Undo and redo history management for Javascript
+ *
+ * Based on the Undo prototype in Mikko Mononen's Stylii:
+ * https://github.com/memononen/stylii/blob/master/js/editor.js#L2
+ *
+ * Copyright(c) 2014 Moniker <jonathan@studiomoniker.com>
+ * MIT Licensed
+ */
+
+(function() {
+	function Undies(param) {
+		this.maxUndos = param.maxUndos || 10;
+		this.restore = param.restore;
+		this.serialize = param.serialize;
+		this.reset();
+	}
+
+	Undies.prototype = {
+		snapshot: function(name) {
+			var state = {
+				name: name,
+				data: this.serialize()
+			};
+
+			// We might be in the middle of the undo history,
+			// so we should discard states after the current one:
+			if (this.head < this.states.length - 1)
+				this.states = this.states.slice(0, this.head + 1);
+
+			this.states.push(state);
+
+			// Remove the oldest state if we have too many states.
+			if (this.states.length > this.maxUndos)
+				this.states.shift();
+
+			this.head = this.states.length - 1;
+		},
+
+		getState: function() {
+			return this.states[this.head];
+		},
+
+		undo: function() {
+			if (this.head < 1)
+				return;
+			this.head--;
+			this.restore(this.getState().data);
+		},
+
+		redo: function() {
+			if (this.head == this.states.length - 1)
+				return;
+			this.head++;
+			this.restore(this.getState().data);
+		},
+
+		reset: function() {
+			this.states = [];
+			this.head = -1;
+		},
+
+		canUndo: function() {
+			return this.head > 0;
+		},
+
+		canRedo: function() {
+			return this.head < this.states.length - 1;
+		}
+	};
+
+	if (typeof exports !== 'undefined') {
+		if (typeof module !== 'undefined' && module.exports) {
+		  exports = module.exports = Undies;
+		}
+		exports.Undies = Undies;
+	} else {
+		this.Undies = Undies;
+	}
+}).call(this);
